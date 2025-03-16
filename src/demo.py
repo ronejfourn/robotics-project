0183fb8a005a4b5f6946f5208a4417478df8142c
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2025-03-16 11:17:35 +0545

    Make nav to ball work

--- src/demo.py
+++ src/demo.py
@@ -11,6 +11,8 @@ import cv2
 from cv_bridge import CvBridge
 import ros2_numpy as r2np
 
+import time
+
 bridge = CvBridge()
 
 class ProcessCameraImage(Node):
@@ -29,10 +31,13 @@ class ProcessCameraImage(Node):
             10,
         )
         self.last_known_pc = None
-        self.publisher = self.create_publisher(PoseStamped, '/goal_pose_test', 10)
+        self.publisher = self.create_publisher(PoseStamped, '/goal_pose', 10)
+        self.last_pub_time = 0
 
     def rgb_listener_callback(self, msg):
         if self.last_known_pc is None:
+            return
+        if (time.time() - self.last_pub_time) < 20.0:
             return
         img_bgr = bridge.imgmsg_to_cv2(msg, 'bgra8')[..., :3]
         img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
@@ -65,6 +70,7 @@ class ProcessCameraImage(Node):
 
         self.publisher.publish(msg)
         self.get_logger().info(f'Published Goal Pose: {msg}')
+        self.last_pub_time = time.time()
 
     def pc_listener_callback(self, msg):
         data = r2np.numpify(msg)
