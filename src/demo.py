1229ebd6f702da8bff362c3b9d6c71c26452d0af
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2025-03-16 11:41:53 +0545

    Make robot stop 0.2units in front of the ball

--- src/demo.py
+++ src/demo.py
@@ -4,7 +4,8 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image, PointCloud2
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
 
 import numpy as np
 import cv2
@@ -30,15 +31,34 @@ class ProcessCameraImage(Node):
             self.pc_listener_callback,
             10,
         )
+
+        # this doesn't work
+        self.pose_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/amcl_pose',
+            self.pose_listener_callback,
+            10,
+        )
+        # this doesn't work either
+        self.odom_sub = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.odom_listener_callback,
+            10,
+        )
         self.last_known_pc = None
         self.publisher = self.create_publisher(PoseStamped, '/goal_pose', 10)
         self.last_pub_time = 0
+        self.pubbed_goal_pose = False
+        self.goal_pose = None
 
     def rgb_listener_callback(self, msg):
         if self.last_known_pc is None:
             return
-        if (time.time() - self.last_pub_time) < 20.0:
+        if self.pubbed_goal_pose:
             return
+        # if (time.time() - self.last_pub_time) < 20.0:
+        #     return
         img_bgr = bridge.imgmsg_to_cv2(msg, 'bgra8')[..., :3]
         img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
         mask_green = cv2.inRange(img_hsv, (36, 25, 25), (70, 255,255))
@@ -60,7 +80,8 @@ class ProcessCameraImage(Node):
         msg.header.frame_id = 'camera_link_optical'
         msg.pose.position.x = self.last_known_pc['x'][cy,cx].item()
         msg.pose.position.y = self.last_known_pc['y'][cy,cx].item()
-        msg.pose.position.z = self.last_known_pc['z'][cy,cx].item()
+        # TODO tune the offset for vacuum suction
+        msg.pose.position.z = self.last_known_pc['z'][cy,cx].item() - 0.3
 
         # coorrect for optical axis being z forward
         msg.pose.orientation.x = -0.5
@@ -70,11 +91,43 @@ class ProcessCameraImage(Node):
 
         self.publisher.publish(msg)
         self.get_logger().info(f'Published Goal Pose: {msg}')
+        self.pubbed_goal_pose = True
         self.last_pub_time = time.time()
+        self.goal_pose = msg.pose
 
     def pc_listener_callback(self, msg):
         data = r2np.numpify(msg)
         self.last_known_pc = data
+
+    def pose_listener_callback(self, msg):
+        current_pose = msg.pose
+        goal_pose = np.array([
+            self.goal_pose.position.x,
+            self.goal_pose.position.y,
+            self.goal_pose.position.z,
+        ])
+        current_pose = np.array([
+            current_pose.position.x,
+            current_pose.position.y,
+            current_pose.position.z,
+        ])
+        if (goal_pose - current_pose).pow(2).sum().pow(0.5) < 0.1:
+            print('were here!')
+
+    def odom_listener_callback(self, msg):
+        current_pose = msg.pose.pose
+        goal_pose = np.array([
+            self.goal_pose.position.x,
+            self.goal_pose.position.y,
+            self.goal_pose.position.z,
+        ])
+        current_pose = np.array([
+            current_pose.position.x,
+            current_pose.position.y,
+            current_pose.position.z,
+        ])
+        if (goal_pose - current_pose).pow(2).sum().pow(0.5) < 0.1:
+            print('were here!')
 
 def main(args=None):
     rclpy.init(args=args)
